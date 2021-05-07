e712603e99f9cbfe6176d4b66418297a75cce267
victoria Van de Weyer <Vdwvictoria@bitbucket.org>
2021-05-07 18:41:45 +0200

    feat("Context.cs ")Add values

--- prbd_2021_a06/Model/Context.cs
+++ prbd_2021_a06/Model/Context.cs
@@ -78,9 +78,59 @@ namespace prbd_2021_a06.Model {
             var Boris = new User("Verh", "Boris", "Password1,", "boris@epfc.eu", Role.Teacher);
             var Penelle = new User("Penelle", "Benoit", "Password1,", "benoit@epfc.eu", Role.Teacher);
             var Bruno = new User("Lacroix", "Bruno", "Password1,", "bruno@epfc.eu", Role.Teacher);
-            Users.AddRange(new[] { Boris, Penelle, Bruno });
+            var hello = new User("hello", "bonjour", "Bonjour1", "hello@bonjour", Role.Student);
+            var letitia = new User("T", "Letitia", "Password1", "letitia@epfc", Role.Student);
+            var victoria = new User("V", "Victoria", "Password1", "hello@epfc", Role.Student);
+            var Nicolas = new User("J", "Nicolas", "Password1", "hello@epfc", Role.Student);
+            var Linh = new User("P", "Linh", "Bonjour1", "hello@epfc", Role.Student);
+           
+            Users.AddRange(new[] { Boris, Penelle, Bruno,hello });
+
+            var PRBD = new Course("PRBD", "projet de developp", 6);
+            var PRWB = new Course("PRWB","projet web", 5);
+            var ANC3 = new Course("ANC3", "Projet de conception", 10);
+            var PRM2 = new Course("PRM2", "Projet de conception", 3);
+            var PRO2 = new Course("PRO2", "Projet de conception", 15);
+            var MATH = new Course("MATH", "Projet de conception", 4);
+            Courses.AddRange(new[] { PRBD, PRWB,ANC3,PRM2,PRO2,MATH });
+            PRBD.Teacher = Penelle;
+            PRWB.Teacher = Penelle;
+            ANC3.Teacher = Bruno;
+            PRM2.Teacher = Boris;
+            PRO2.Teacher = Bruno;
+            MATH.Teacher = Boris;
+
+            var studentCourse = new StudentCourse();
+            var studentCourseL = new StudentCourse();
+            var studentCourseV = new StudentCourse();
+            var studentCourseN = new StudentCourse();
+            var studentCourseLi = new StudentCourse();
+
+            studentCourse.Student = hello;
+            studentCourse.Course = PRBD;
+            studentCourse.IsValide = true;
+
+            studentCourseL.Student = letitia;
+            studentCourseL.Course = PRBD;
+            studentCourseL.IsActif = true;
+
+            studentCourseV.Student = victoria;
+            studentCourseV.Course = PRWB;
+            studentCourseV.IsActif = true;
+
+            studentCourseN.Student = Nicolas;
+            studentCourseN.Course = MATH;
+            studentCourseN.IsActif = false;
+
+            studentCourseLi.Student = Linh;
+            studentCourseLi.Course = PRO2;
+            studentCourseLi.IsValide = false;
+
+            StudentCourses.AddRange(new[] {studentCourse,studentCourseL,studentCourseN, studentCourseLi,studentCourseV });
             SaveChanges();
             Database.CommitTransaction();
+
+
         }
 
         public DbSet<User> Users { get; set; } 
@@ -104,11 +154,25 @@ namespace prbd_2021_a06.Model {
         ) {
             Database.BeginTransaction();
             var user = new User(FirstName,LastName,Password,Email);
-            Console.WriteLine(user);
             Users.AddRange(user);
             SaveChanges();
             Database.CommitTransaction();
             return user;
+        }
+        public Course CreateCourse(
+            string Title,
+            string Description,
+            int MaxStudent
+            )
+        {
+            Database.BeginTransaction();
+            var course = new Course(Title, Description, MaxStudent
+                );
+            course.Teacher = App.CurrentUser;
+            Courses.AddRange(course);
+            SaveChanges();
+            Database.CommitTransaction();
+            return course;
         }
     }
 }
